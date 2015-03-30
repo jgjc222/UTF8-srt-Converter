cea4dfede9bc4d245a43f49e37dd24371eb8641c
Abdulaziz Nasser <jgjc222@users.noreply.github.com>
2015-03-30 03:58:26 +0300

    Update Converter.ps1

--- Converter.ps1
+++ Converter.ps1
@@ -1,11 +1,11 @@
-$path = $name = Read-Host 'Enter Directory'
+$path = Read-Host 'Dir'
 write-Host "Recursively? (y,n)"
 $confirmation = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 if ($confirmation.VirtualKeyCode -eq 89) {
-$files = [IO.Directory]::GetFiles($name,"*.srt", [System.IO.SearchOption]::AllDirectories)
+$files = [IO.Directory]::GetFiles($path,"*.srt", [System.IO.SearchOption]::AllDirectories)
 }
 elseif  ($confirmation.VirtualKeyCode -eq 78) {
-$files = [IO.Directory]::GetFiles($name,"*.srt")
+$files = [IO.Directory]::GetFiles($path,"*.srt")
 }
 else{
 "Terminating.."
